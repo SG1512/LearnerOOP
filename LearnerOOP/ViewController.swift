209c7b73d2b12b32cb081b941a419d643372e93a
Steven Gunawan <stevengunawan1512@gmail.com>
2019-05-15 14:19:10 +0700

    -Passes CD 10 & CD 17 Learning Objectives

--- LearnerOOP/ViewController.swift
+++ LearnerOOP/ViewController.swift
@@ -9,11 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var namaLabel: UILabel!
+    @IBOutlet weak var umurLabel: UILabel!
+    @IBOutlet weak var genderLabel: UILabel!
+     
+    
+    
+    var learnerInstance: LearnerModel? // You just create copy of LearnerModerl
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    learnerInstance =
+        LearnerModel(nameLearner: "SG", ageLearner: 20, genderLearner: "Male", imageProfileLearner: "")
+    updateUI()
     }
 
+    func updateUI(){
+        if let instance = learnerInstance {
+            namaLabel.text = instance.name
+            umurLabel.text = "\(instance.age)"
+            genderLabel.text = instance.gender
+        }
+    }
 
+    @IBAction func buttonDiPencet(_ sender: UIButton) {
+        if let instance = learnerInstance {
+            instance.increaseAge()
+            updateUI()
+        }
+    }
+    
 }
 
